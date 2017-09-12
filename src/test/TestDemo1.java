8cced536df42280276aac6ae6aa9845668d6c16a
10599 <yuanbo@zbj.com>
2017-09-12 16:51:51 +0800

    master

---
+++ src/test/TestDemo1.java
@@ -0,0 +1,7 @@
+package test;
+
+public class TestDemo1 {
+ public static void main(String [] args){
+	 System.out.print("dskjmd");
+ }
+}
---
+++ src/test/Zhuanhuan.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.sun.javafx.collections.MappingChange.Map;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+
+
+public class Zhuanhuan{
+	  public static void main(String[] args){
+		  HashMap<String, Object> map = new HashMap<String, Object>();  
+		    map.put("1", "fds");  
+		    map.put("2", "valu");  
+		    List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>(); 
+	}
